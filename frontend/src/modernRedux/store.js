c530d9b9f829d54e8143aefeda7c9adc45676b78
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2022-05-01 01:30:53 -0700

    Modern Redux

---
+++ frontend/src/modernRedux/store.js
@@ -0,0 +1,14 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import cartReducer from "./cartRedux";
+
+
+const rootReducer = combineReducers({ cart: cartReducer });
+
+
+export const store = configureStore({
+  reducer: rootReducer
+
+});
+
+export default store;
+
