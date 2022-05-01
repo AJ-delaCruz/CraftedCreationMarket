8d6c2a47f6bae55a1c5b8a5de80897ebc5892a6b
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2022-05-01 01:34:33 -0700

    added link to categories

--- frontend/src/components/Product/Category.js
+++ frontend/src/components/Product/Category.js
@@ -1,42 +1,57 @@
 import React, {Component} from 'react';
 import {categories} from "./data";
 import {Image} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 class Category extends Component {
 
 
     render() {
 
-        let details = categories.map((x, key) => {
+        let details = categories.map((x)=> {
             return (
-                <div >
+
+
+                <div key ={x.id} style={{
+                    flex: "1",
+                    margin: "3px",
+                    // height: "10vh",
+                    position: "relative",
+                }}>
                     <img src={x.img} style={{
-                        width: "40%",
-                        height: "50%",
+                        width: "80%",
+                        height: "80%",
                         objectFit: "cover",
-                        // display: "flow",
-                        // padding: "10px",
-                        // flexDirection: "column"
+                        borderRadius: "50%"
+
 
                     }}/>
-
-                    <div>{x.title}</div>
-
+                    <Link to={`/productList/${x.title}`}>
+                    <div style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        marginRight: "45px"
+                    }}>
+                        {x.title}
+                    </div>
+                    </Link>
 
                 </div>
+
             )
         })
 
         return (
-            <div>
-                Category
-                <div className="container">
-                    <h2>Category</h2>
-                    <div style={{display:"flex"}}>
-                        {details }
-                    </div>
 
 
+            <div className="container">
+                <h2>Category</h2>
+                <div style={{
+                    display: "flex",
+                    padding: "20px",
+                    justifyContent: "space-between"
+                }}>
+                    {details}
                 </div>
             </div>
         );
