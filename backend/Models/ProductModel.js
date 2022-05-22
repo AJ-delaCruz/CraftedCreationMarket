5f69c6940a5f9f6696c8d973f89254ac262618d2
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2022-05-22 04:47:47 -0700

    Models update

--- backend/Models/ProductModel.js
+++ backend/Models/ProductModel.js
@@ -2,16 +2,32 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
     {
-        sellerId: {type: String, required: true, unique: true},
-        name: {type: String, required: true, unique: true},
+        sellerId: {type: String, required: true},
+        shopName: {type: String, required: true},
+        // name: {type: String, required: true},
+        // categories: {type: Array},
+        // description: {type: String, required: true,},
+        // img: {type: String, required: true},
+        // price: {type: Number, required: true},
+        // inStock: { type: Boolean, default: true }
+        title: {type: String, required: true,},
+        description: {type: String},
+        img: {type: String},
         categories: {type: Array},
-        description: {type: String, required: true,},
-        img: {type: String, required: true},
+        quantity: {type: Number, required: true},
         price: {type: Number, required: true},
+        inStock: {type: Boolean},
+        sale: {type: Number, default: 0}
+        // color: {type: Array},
+        // price: {type: Number, required: true},
+        // size: {type: Array},
+        // favorites: {type: Array}
+
 
     },
     {
-            versionKey: false
+        versionKey: false,
+        timestamps: true
     }
 );
 
