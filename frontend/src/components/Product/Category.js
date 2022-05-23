55e340b304742fca9ce9f988dfe1396e8808d5b0
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2022-05-22 17:22:37 -0700

    Link to products with a category

--- frontend/src/components/Product/Category.js
+++ frontend/src/components/Product/Category.js
@@ -8,32 +8,37 @@ class Category extends Component {
 
     render() {
 
-        let details = categories.map((x)=> {
+        let details = categories.map((x) => {
             return (
 
 
-                <div key ={x.id} style={{
+                <div key={x.id} style={{
                     flex: "1",
                     margin: "3px",
                     // height: "10vh",
                     position: "relative",
                 }}>
-                    <img src={x.img} style={{
-                        width: "80%",
-                        height: "80%",
-                        objectFit: "cover",
-                        borderRadius: "50%"
+                    <Link to={`/productList/${x.title}`}
+                          style={{color: "black", textDecoration: "none"}}
+                    >
+
+                        <img src={x.img} style={{
+                            width: "80%",
+                            height: "80%",
+                            objectFit: "cover",
+                            borderRadius: "50%"
 
 
-                    }}/>
-                    <Link to={`/productList/${x.title}`}>
-                    <div style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        marginRight: "45px"
-                    }}>
-                        {x.title}
-                    </div>
+                        }}/>
+
+
+                        <div style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            marginRight: "45px",
+                        }}>
+                            {x.title}
+                        </div>
                     </Link>
 
                 </div>
