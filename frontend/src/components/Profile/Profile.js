b18e8d7e77543ce90e3193d8b8a1b43dc6f05b67
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2022-05-27 22:19:44 -0700

    seller and user profile page

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,146 +1,102 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import Navbar from "../LandingPage/Navbar";
-import axios from "axios";
 import {Link, NavLink} from "react-router-dom";
-import Footer from "../Footer/Footer";
-import Favorites from "../Favorites/Favorites"
+import {useState} from "react";
+import axios from "axios";
+import Favorite from "../Favorites/Favorite";
+// import Footer from "../Footer/Footer";
+// import Favorite from "../Favorites/Favorite"
 
-class Profile extends Component {
-    constructor(props) {
-        super(props);
+const Profile = () => {
+    const userId = localStorage.getItem("user_id");
+    const [user, setUser] = useState("");
 
-        this.state = {
-            name: "",
-            authFlag: false,
-            errorMsg: null,
-        };
+    useEffect(() => {
+        const findUser = async () => {
+            try {
+                const res = await axios.get(`http://localhost:3001/user/find?userId=${userId}`);
+                setUser(res.data);
+                console.log(res.data);
 
-        // this.submitSignup = this.submitSignup.bind(this);
-        // this.passwordChangeHandler = this.passwordChangeHandler.bind(this);
-        // this.usernameChangeHandler = this.usernameChangeHandler.bind(this);
-
-    }
-
-    componentDidMount() {
-        axios.get('http://localhost:3001/profile')
-            .then((response) => {
-                console.log("profile page")
-                console.log(response.data);
-                this.setState({
-                    name: response.data[0].name
-
-                });
-                console.log(response.data[0].name)
-            });
-    }
-
-    submitSignup(e) {
-        //prevent page from refresh
-        e.preventDefault();
-        const data = {
-
-            name: this.state.name,
-            // phoneNum: this.state.phoneNum,
-        }
-        //set the with credentials to true
-        axios.defaults.withCredentials = true;
-        //make a post request with the user data
-        axios.post('http://localhost:3001/signup', data)
-            .then(response => {
-                console.log("Status Code : ", response.status);
-                if (response.status === 200) {
-                    this.setState({
-                        authFlag: true
-                    })
-                } else {
-                    this.setState({
-                        authFlag: false
-                    })
-                }
-            })
-            .catch(err => {
+            } catch (err) {
                 console.log(err);
-                //set invalid message
-                this.setState({
-                    errorMsg: "Invalid."
-                });
-            });
-
-    }
-
-    render() {
-        return (
-            <div>
-                {<Navbar/>}
-                <div className="container">
-                    {/*</div>*/}
+            }
+        };
+        findUser();
+    }, [userId]);
+    console.log(user);
+    return (
+        <div>
+            {<Navbar/>}
+            <div className="container">
+                {/*</div>*/}
 
 
-                    <div className="profile" style={{
+                <div className="profile" style={{
+                    margin: "10px",
+                    // height: "300px",
+                    alignItems: 'center',
+                    justifyContent: "space-between",
+                    display: 'flex'
+                }}>
+                    <div style={{
                         margin: "10px",
-                        // height: "300px",
                         alignItems: 'center',
-                        justifyContent: "space-between",
                         display: 'flex'
                     }}>
-                        <div style={{
-                            margin: "10px",
-                            alignItems: 'center',
-                            display: 'flex'
-                        }}>
-                            <img
-                                style={{width: '150px', height: '120px',}}
-                                alt=""
-                                // src={
-                                //     this.state.ProfileImg !== null &&
-                                //     this.state.ProfileImg.length > 0
-                                //         ? this.state.ProfileImg
-                                //         : "../images/defaultProfilePic.png"
-                                // }
-                                src="../images/defaultProfilePic.png"
-                            />
-                            <h1 style={{marginLeft: "30px"}}>
-                                {this.state.name}
-
-                                <Link to="/profileUpdate">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                         className="bi bi-pencil-square" viewBox="0 0 16 16"
-                                         style={{marginLeft: "10px", color: "black"}}>
-                                        <path
-                                            d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                        <path fill-rule="evenodd"
-                                              d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                    </svg>
-                                </Link>
-                            </h1>
-                        </div>
-                    </div>
-
-
-                    <div className="favorites" style={{
-
-                        // flex: "1",
-                        margin: "10px",
-
-                        // alignItems: 'center',
-                        justifyContent: "space-between",
-                        display: 'flex',
-                        // backgroundColor: "#f5fbfd",
-                        // position: "relative",
-                    }}>
-
-
-                    </div>
-                    <div style={{marginBottom: "50px"}}>
-                        {<Favorites/>}
+                        <img
+                            style={{width: '150px', height: '120px',}}
+                            alt=""
+                            // src={
+                            //     this.state.ProfileImg !== null &&
+                            //     this.state.ProfileImg.length > 0
+                            //         ? this.state.ProfileImg
+                            //         : "../images/defaultProfilePic.png"
+                            // }
+                            src="../image/defaultProfilePic.png"
+                        />
+                        <h1 style={{marginLeft: "30px"}}>
+                            {/*{this.state.name}*/}
+                            {user.name ? user.name : user.username}
+                            <Link to="/profileUpdate">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                     className="bi bi-pencil-square" viewBox="0 0 16 16"
+                                     style={{marginLeft: "10px", color: "black"}}>
+                                    <path
+                                        d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                    <path fill-rule="evenodd"
+                                          d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                </svg>
+                            </Link>
+                        </h1>
                     </div>
                 </div>
 
-                {<Footer/>}
+
+                <div className="favorites" style={{
+
+                    // flex: "1",
+                    margin: "10px",
+
+                    // alignItems: 'center',
+                    justifyContent: "space-between",
+                    display: 'flex',
+                    // backgroundColor: "#f5fbfd",
+                    // position: "relative",
+                }}>
+
+
+                </div>
+                <div style={{marginBottom: "50px"}}>
+                    {<Favorite/>}
+                </div>
             </div>
 
-        );
-    };
+            {/*{<Footer/>}*/}
+        </div>
+
+    );
+
 };
 
 //export Profile Component
--- frontend/src/components/Shop/Seller.js
+++ frontend/src/components/Shop/Seller.js
@@ -1,186 +1,110 @@
 import React, {Component} from 'react';
-import Footer from "../Footer/Footer";
-import Navbar from "../LandingPage/Navbar";
-import axios from "axios";
+// import Footer from "../Footer/Footer";
+// import Navbar from "../LandingPage/Navbar";
+// import axios from "axios";
 import {Link} from "react-router-dom";
-import Favorites from "../Favorites/Favorites";
+// import Favorite from "../Favorites/Favorite";
 import {popularProducts} from "../Product/data";
 import {sellerItems} from "./data";
 import Product from "../Product/Product";
+import {useState} from "react";
+import {useEffect} from "react";
+import axios from "axios";
 
-class Seller extends Component {
-    constructor(props) {
-        super(props);
+const Seller = () => {
+    const [searchProduct, setSearchProduct] = useState("");
+    const [sellerProduct, setSellerProduct] = useState([]);
 
-        this.state = {
-            name: "AJ",
-            shopName: "SHOP NAME",
-            imageUrl: null,
-            authFlag: false,
-            errorMsg: null,
-            sellers: [{shopNames: null}],
-            searchProduct: "",
-            favoriteItems:[],
+    const userId = localStorage.getItem("user_id");
+
+
+    useEffect(() => {
+        const findSellerProduct = async () => {
+            try {
+                const res = await axios.get(`http://localhost:3001/products/seller?sellerId=${userId}`);
+                setSellerProduct(res.data);
+                console.log(res.data);
+
+            } catch (err) {
+                console.log(err);
+            }
         };
+        findSellerProduct();
+    },[userId]);
 
-        // this.submitSignup = this.submitSignup.bind(this);
-        // this.passwordChangeHandler = this.passwordChangeHandler.bind(this);
-        // this.emailChangeHandler = this.emailChangeHandler.bind(this);
-        this.searchHandler = this.searchHandler.bind(this);;
-        this.submitSearch = this.submitSearch.bind(this);
-
-    }
-
-    componentDidMount() {
-        console.log(this.state.sellers);
-        console.log(this.state.sellers.map(x => x.shopNames));
-        console.log(this.state.sellers.some(x => x.shopNames === null));
-        // axios.get('http://localhost:3001/home')
-        //     .then((response) => {
-        //         //update the state with the response data
-        //         console.log(response);
-        //         this.setState({
-        //             sellers: this.state.sellers.concat(response.data)
-        //         });
-        //     });
-    }
-
-    searchHandler = (e) => {
-        this.setState({
-            searchProduct: e.target.value
-        })
-    }
-
-    //submit Login handler to send a request to the node backend
-    submitSearch = async (e) => {
-        e.preventDefault();
-        const data = {
-            searchProduct: this.state.searchProduct,
-        }
-
-        const findItem = sellerItems.filter(x => x.name === this.state.searchProduct);
-        console.log(findItem);
-        console.log(this.state.authFlag);
-        this.setState({
-            authFlag: true,
-            favoriteItems: popularProducts.filter(x => x.name === this.state.searchProduct)
-        })
-        console.log(this.state.authFlag);
-        console.log("Favorites");
-        console.log(this.state.favoriteItems);
-        // {popularProducts.map((findItem) => (
-        //     <Product item={findItem} key={findItem.id}/>
-        // ))}
-        // axios.get('http://localhost:3001/favorites', data)
-        //     .then(response => {
-        //         console.log("Status Code : ", response.status);
-        //         if (response.status === 200) {
-        //             this.setState({
-        //                 authFlag: true
-        //             })
-        //         } else {
-        //             this.setState({
-        //                 authFlag: false
-        //             })
-        //         }
-        //     })
-        //     .catch(err => {
-        //         console.log(err);
-        //         this.setState({
-        //             errorMsg: "No item found"
-        //         });
-        //     });
-    }
-
-    render() {
-
-        const newShop = () => {
-            return (
-                <div>
-                    <h1> newSHop</h1>
-                </div>
-            )
-
-        }
-        return (
+    return (
+        <div>
+            {/*{!this.state.sellers.find(x => x.shopNames === null) === false ?*/}
             <div>
-                {/*{newShop()}*/}
-                {!this.state.sellers.find(x => x.shopNames === null) === false ?
+                <div className="profileTopBar" style={{
+                    margin: "10px",
+                    alignItems: 'center',
+                    display: 'flex',
+                    justifyContent: "space-between",
+
+                }}>
+
                     <div>
-                    <div class="profileTopBar" style={{
-                        margin: "10px",
-                        alignItems: 'center',
-                        display: 'flex',
-                        justifyContent: "space-between",
+                        <h2 style={{marginLeft: "30px"}}>
+                            Seller Items
 
-                    }}>
-
-                        <div>
-                            <h2 style={{marginLeft: "30px"}}>
-                                Seller Items
-
-                                <Link to="/profileUpdate">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                         className="bi bi-pencil-square" viewBox="0 0 16 16"
-                                         style={{marginLeft: "10px", color: "black"}}>
-                                        <path
-                                            d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                        <path fill-rule="evenodd"
-                                              d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                    </svg>
-                                </Link>
-                            </h2>
-                        < /div>
+                            <Link to="/profileUpdate">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                     className="bi bi-pencil-square" viewBox="0 0 16 16"
+                                     style={{marginLeft: "10px", color: "black"}}>
+                                    <path
+                                        d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                    <path fill-rule="evenodd"
+                                          d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                </svg>
+                            </Link>
+                        </h2>
+                    < /div>
 
 
-                        <div style={{display: 'flex', justifyContent: "right"}}>
-                            <form style={{display: 'flex'}}  onSubmit={this.submitSearch}>
-                                <input onChange={this.searchHandler} placeholder="Search items"/>
-                                <div className="btn btn-light btn-outline-secondary ">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="30"
-                                        height="16"
-                                        fill="currentColor"
-                                        className="bi bi-search "
-                                        viewBox="0 0 16 16"
+                    <div style={{display: 'flex', justifyContent: "right"}}>
+                        <form style={{display: 'flex'}} >
+                            {/*// onSubmit={submitSearch}>*/}
+                            <input onChange={(e) => {
+                                setSearchProduct(e.target.value);
+                            }} placeholder="Search items"/>
+                            <div className="btn btn-light btn-outline-secondary ">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="30"
+                                    height="16"
+                                    fill="currentColor"
+                                    className="bi bi-search "
+                                    viewBox="0 0 16 16"
 
-                                    >
-                                        <path
-                                            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                                    </svg>
-                                </div>
-                            </form>
+                                >
+                                    <path
+                                        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                </svg>
+                            </div>
+                        </form>
 
-                            <select className="Filter">
-                                <option selected>Filter</option>
-                                <option>Price (asc)</option>
-                                <option>Price (desc)</option>
-                            </select>
-
-                        </div>
-                    </div>
-
-
-                    <div className="container" style={{flexWrap: "wrap", alignItems: 'center', display: "flex"}}>
-
-                        {this.state.searchProduct !== "" ? this.state.favoriteItems.map((x, key) => (
-                                <Product item={x} key={x.id}/>
-                            ))
-                            :
-                            sellerItems.map((x, key) => (
-                                <Product item={x} key={x.id}/>
-                            ))
-                        }
-                    </div>
+                        <select className="Filter">
+                            <option selected>Filter</option>
+                            <option>Price (asc)</option>
+                            <option>Price (desc)</option>
+                        </select>
 
                     </div>
+                </div>
 
-                    : newShop()}
+
+                <div className="container" style={{flexWrap: "wrap", alignItems: 'center', display: "flex"}}>
+
+                    {sellerProduct.map((item) => <Product item={item} key={item.id}/>)}
+                </div>
+
             </div>
-        )
 
-    }
+            {/*: newShop()}*/}
+        </div>
+    )
+
 }
 
 export default Seller;
