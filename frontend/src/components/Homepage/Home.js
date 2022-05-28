e375ee42929163d15753cc98f3660011fcfd705a
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2022-05-27 22:10:59 -0700

    user's name on home page

--- frontend/src/components/Homepage/Home.js
+++ frontend/src/components/Homepage/Home.js
@@ -4,8 +4,26 @@ import Navbar from "../LandingPage/Navbar";
 import Category from "../Product/Category";
 import ProductList from "../Product/ProductList";
 import Footer from "../Footer/Footer";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const Home = () => {
+    const userId = localStorage.getItem("user_id");
+    const [name, setName] = useState("");
+
+    useEffect(() => {
+        const findName = async () => {
+            try {
+                const res = await axios.get(`http://localhost:3001/user/find?userId=${userId}`);
+                setName(res.data.name);
+                console.log(res.data.name);
+
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        findName();
+    },[userId]);
 
     let redirectVar = null;
     // if(!cookie.load('cookie')){
@@ -16,8 +34,10 @@ const Home = () => {
             {/*{redirectVar} */}
 
             <Navbar/>
+            <h2 style = {{marginTop:"30px", marginLeft:"300px"}}> Welcome back, {name}</h2>
             <Category/>
-            {/*<ProductList/>*/}
+            <div style = {{padding: "110px", marginTop:"-130px"}}>  <ProductList/></div>
+
             <Footer/>
 
         </div>
