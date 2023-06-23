28c7a387c724ae6b8aae3719df29642241e4507a
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-06-23 15:59:31 -0700

    fix user welcome, add uncommited user API endpoints from last year

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,12 +2,12 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const {secret} = require('../Utils/config');
+const { secret } = require('../Utils/config');
 const User = require('../Models/UserModel');
-const {auth} = require("../utils/passport");
+const { auth } = require("../utils/passport");
 const bcrypt = require('bcrypt');
 
-auth();
+// auth();
 
 //signup
 router.post('/signup', async (req, res) => {
@@ -25,18 +25,18 @@ router.post('/signup', async (req, res) => {
     console.log(req.body.username);
     console.log(req.body.password);
 
-    User.findOne({username: req.body.username}, (err, userTaken) => {
+    User.findOne({ username: req.body.username }, (err, userTaken) => {
         if (err) {
             console.log(err);
             console.log("ERROR SIGNING UP");
-            res.status(500).send({ message: err});
+            res.status(500).send({ err });
         }
         if (userTaken) {
-            res.status(409).send({ message: "Username already exists"});
+            res.status(409).send("Username already exists");
         } else {
             newUser.save((error, data) => {
                 if (error) {
-                    res.status(500).send({ message: error});
+                    res.status(500).send({ error });
                 } else {
                     console.log("sign up works");
                     res.status(200).send(data);
@@ -58,7 +58,7 @@ router.post('/login', async (req, res) => {
     console.log(usernameValue);
     console.log(passwordValue);
 
-    User.findOne({username: req.body.username}, async (error, user) => {
+    User.findOne({ username: req.body.username }, async (error, user) => {
         if (error) {
             console.log("LOGIN NOT WORKING");
             res.writeHead(500, {
@@ -72,7 +72,7 @@ router.post('/login', async (req, res) => {
             await bcrypt.compare(req.body.password, user.password, (err, result) => {
                 if (err) {
                     console.log(err);
-                    res.send({error: err});
+                    res.send({ error: err });
                 }
                 if (result) {
                     // if (!credentialsValid) {
@@ -80,7 +80,7 @@ router.post('/login', async (req, res) => {
                     // }
                     console.log("SUCCESSFUL LOGIN");
                     //jwt
-                    const payload = {_id: user._id, username: user.username,};
+                    const payload = { _id: user._id, username: user.username, };
                     const token = jwt.sign(payload, secret, {
                         expiresIn: 1008000
                     });
@@ -109,35 +109,30 @@ router.post('/login', async (req, res) => {
 
 
 //update profile
-router.put("/update/:id", async (req, res) => {
+router.put("/update", async (req, res) => {
+    const currentUser = await User.findById(req.body.userId);
+    console.log(currentUser);
+    const newUser = { ...req.body };
+    console.log(newUser);
 
-    // await User.findByIdAndUpdate( req.params.id, { $set: req.body, }, { new: true },
-    //     (err, result) =>
-    //     {
-    //         if (err) {
-    //             console.log(err);
-    //             console.log("ERROR UPDATING PROFILE");
-    //             res.writeHead(400, {
-    //                 'Content-Type': 'text/plain'
-    //             })
-    //             res.end("ERROR UPDATING PROFILE");
-    //         } else {
-    //             // res.send(result);
-    //             console.log("profile update works");
-    //
-    //             res.status(200).json(result);
-    //             res.end("Successful PROFILE UPDATE");
-    //         }
-    //
-    //
-    //     });
     try {
         const updateUser = await User.findByIdAndUpdate(
-            req.params.id,
+            req.body.userId,
             {
-                $set: req.body,
+                $set:
+                    // name: req.body.name,
+                    // img: req.body.image,
+                    // street: req.body.street,
+                    // city: req.body.city,
+                    // state: req.body.state,
+                    // country: req.body.country,
+                    // zipCode: req.body.zipCode,
+                    // email: req.body.email,
+                    // phoneNum: req.body.phoneNum,
+                    // birthDay: req.body.birthDay,
+                    newUser
+
             },
-            {new: true}
         );
         console.log("profile update works");
         res.status(200).json(updateUser);
@@ -150,6 +145,47 @@ router.put("/update/:id", async (req, res) => {
     }
 
 
+});
+
+
+//find user details
+router.get("/find", async (req, res) => {
+    console.log("INSIDE USER FIND");
+    const userId = req.query.userId;
+    try {
+        const user = await User.findById(userId);
+        res.status(200).json(user);
+        console.log("SUCCESS GET USER")
+
+    } catch (err) {
+        console.log("ERROR GET USER");
+        res.status(500).json(err);
+    }
+});
+
+//create shop
+router.put("/createShop", async (req, res) => {
+    console.log("INSIDE SHOP Create");
+    const userId = req.body.userId;
+    const newShop = req.body.shopName;
+
+    console.log(userId);
+    console.log(newShop);
+    try {
+        const shopExist = await User.findOne({ shopName: newShop });
+        console.log(shopExist);
+        if (!shopExist) {
+            const createShop = await User.findByIdAndUpdate(
+                userId, { shopName: newShop }, { new: true }
+            )
+            console.log("new shop created successfully");
+            res.status(200).send(createShop);
+        } else {
+            res.status(400).send("Shop Name already exists");
+        }
+    } catch (err) {
+        res.status(409).send(err);
+    }
 });
 
 
--- frontend/src/components/Homepage/Home.js
+++ frontend/src/components/Homepage/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Navigate} from 'react-router';
+import { Navigate } from 'react-router';
 import Navbar from "../LandingPage/Navbar";
 import Category from "../Product/Category";
 import ProductList from "../Product/ProductList";
 import Footer from "../Footer/Footer";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Home = () => {
@@ -16,29 +16,29 @@ const Home = () => {
             try {
                 const res = await axios.get(`http://localhost:3001/user/find?userId=${userId}`);
                 setName(res.data.name);
-                console.log(res.data.name);
+                // console.log("Name " + res.data.name);
 
             } catch (err) {
                 console.log(err);
             }
         };
         findName();
-    },[userId]);
+    }, [userId]);
 
     let redirectVar = null;
     // if(!cookie.load('cookie')){
     //     redirectVar = <Navigate to= "/login"/>
     // }
-    return(
+    return (
         <div >
             {/*{redirectVar} */}
 
-            <Navbar/>
-            <h2 style = {{marginTop:"30px", marginLeft:"300px"}}> Welcome back, {name}</h2>
-            <Category/>
-            <div style = {{padding: "110px", marginTop:"-130px"}}>  <ProductList/></div>
+            <Navbar />
+            <h2 style={{ marginTop: "30px", marginLeft: "300px" }}>  {name ? "Welcome back, " + name : ""}</h2>
+            <Category />
+            <div style={{ padding: "110px", marginTop: "-130px" }}>  <ProductList /></div>
 
-            <Footer/>
+            <Footer />
 
         </div>
     )
