b960ae2b2eef86ceca1122399a182883f9ee86de
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-06-23 15:34:25 -0700

    add shared-library to reuse models across different repos

---
+++ shared-library/models/ProductModel.js
@@ -0,0 +1,26 @@
+const initProductModel = (mongoose) => {
+    const ProductSchema = new mongoose.Schema(
+        {
+            sellerId: { type: String, required: true },
+            // shopName: { type: String, required: true },
+            title: { type: String, required: true, },
+            description: { type: String },
+            img: { type: String },
+            categories: { type: Array },
+            quantity: { type: Number, required: true },
+            price: { type: Number, required: true },
+            inStock: { type: Boolean },
+            sale: { type: Number, default: 0 }
+
+        },
+        {
+            versionKey: false,
+            timestamps: true
+        }
+
+    );
+
+    return mongoose.model("Product", ProductSchema);
+};
+
+module.exports = initProductModel;
